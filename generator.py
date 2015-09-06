b916d95f9b1e8f1323c8c00a34ec3d3e0ab87fbb
Tomasz Galecki <tomasz.galecki@hotmail.com>
2015-09-06 19:13:51 +0200

    List class: add_new_person() added

--- generator.py
+++ generator.py
@@ -5,7 +5,7 @@ class Attribute(object):
     """
     Stores an indexes of an attribute used in lists of readers.
     """
-    id = 0
+    person_id = 0
     name = 1
     lection = 2
     psalm = 3
@@ -20,7 +20,7 @@ class List(object):
     to this function. value >= 0 describing readiness to be appointed
     """
     def __init__(self):
-        self.list = None
+        self.list = []
         self.load_list()
 
     def load_list(self):
@@ -28,3 +28,22 @@ class List(object):
         with open("list.json") as file:
             self.list = json.loads(file.read())
 
+    def add_new_person(self, name, lection, psalm, believers_pray):
+        """Add new person at the end of self.list and update 'list.json'."""
+        name_validity = isinstance(name, str)
+        lection_validity = isinstance(lection, int)
+        psalm_validity = isinstance(psalm, int)
+        believers_pray_validity = isinstance(believers_pray, int)
+
+        if name_validity and lection_validity and psalm_validity and believers_pray_validity:
+            # get last id from list (if exist) and increment
+            if len(self.list) > 0:
+                new_id = self.list[-1][0] + 1
+            else:
+                new_id = 0
+
+            self.list.append([new_id, name, lection, psalm, believers_pray])
+            self.file_list_update()
+            return True
+        else:
+            return False
