22e622e87b03341623d6539efec90c3a21e47d49
Tomasz Galecki <tomasz.galecki@hotmail.com>
2015-09-06 17:33:19 +0200

    Attribute class added

---
+++ generator.py
@@ -0,0 +1,9 @@
+class Attribute(object):
+    """
+    Stores an indexes of an attribute used in lists of readers.
+    """
+    id = 0
+    name = 1
+    lection = 2
+    psalm = 3
+    believers_pray = 4
