46c14835c0cbc05fa7728f7776ea88dbaa92f8ad
Tomasz Galecki <tomasz.galecki@hotmail.com>
2015-09-06 18:15:04 +0200

    in List class: init and load_list added

--- generator.py
+++ generator.py
@@ -11,3 +11,18 @@ class Attribute(object):
     psalm = 3
     believers_pray = 4
 
+
+class List(object):
+    """
+    List of available people and their lection preferences.
+    Attributes of each line in 'self.list' are the same as in 'Attribute' class.
+
+    """
+    def __init__(self):
+        self.list = None
+        self.load_list()
+
+    def load_list(self):
+        """Loads data from 'list.json' and place it into 'self.list'."""
+        with open("list.json") as file:
+            self.list = json.loads(file.read())
