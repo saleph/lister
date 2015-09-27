1fca6ece9977541a672280c7f339aa58a28d1f2f
Tomasz Galecki <tomasz.galecki@hotmail.com>
2015-09-27 18:12:41 +0200

    registred Reader, Day and Mess models -> admin.py

--- admin.py
+++ admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Reader, Day, Mess
 
-# Register your models here.
+
+class ReaderAdmin(admin.ModelAdmin):
+    fields = [
+        'name',
+        'wants_lection',
+        'wants_psalm',
+        'wants_believers_pray',
+        'owner'
+    ]
+
+
+class DayAdmin(admin.ModelAdmin):
+    fields = [
+        'date',
+        'second_lection_exist',
+        'owner'
+    ]
+
+
+class MessAdmin(admin.ModelAdmin):
+    fields = [
+        'day',
+        'hour',
+        'first_lection',
+        'second_lection',
+        'psalm',
+        'believers_pray'
+    ]
+
+
+admin.site.register(Reader, ReaderAdmin)
+admin.site.register(Day, DayAdmin)
+admin.site.register(Mess, MessAdmin)
