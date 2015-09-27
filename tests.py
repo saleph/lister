be3959a0af72cd55a249e21f56121f054d26b9ea
Tomasz Galecki <tomasz.galecki@hotmail.com>
2015-09-27 18:07:50 +0200

    added tests for Reader.at_least_one_wants_is_true

--- tests.py
+++ tests.py
@@ -3,6 +3,11 @@ from .models import Reader
 
 
 class ReaderMethodTests(TestCase):
+    """
+    Tests of:
+    1. reset_speech_number()
+    2. at_least_one_wants_is_true()
+    """
     def test_reset_speech_number_with_zero_value(self):
         """
         reset_speech_number() should return 0 for readers whose
@@ -29,3 +34,36 @@ class ReaderMethodTests(TestCase):
         reader = Reader(speech_number=5)
         reader.reset_speech_number()
         self.assertEqual(reader.speech_number, 1)
+
+    def test_at_least_one_wants_is_true_with_three_false(self):
+        """
+        at_least_one_wants_is_true() should return False
+        if all the wants are False.
+        """
+        reader = Reader()
+        self.assertEqual(reader.at_least_one_wants_is_true(), False)
+
+    def test_at_least_one_wants_is_true_with_two_false_one_true(self):
+        """
+        at_least_one_wants_is_true() should return True
+        if one wants are True.
+        """
+        reader = Reader(wants_lection=True)
+        self.assertEqual(reader.at_least_one_wants_is_true(), True)
+
+    def test_at_least_one_wants_is_true_with_one_false_two_true(self):
+        """
+        at_least_one_wants_is_true() should return True
+        if two wants are True.
+        """
+        reader = Reader(wants_lection=True, wants_psalm=True)
+        self.assertEqual(reader.at_least_one_wants_is_true(), True)
+
+    def test_at_least_one_wants_is_true_with_three_true(self):
+        """
+        at_least_one_wants_is_true() should return True
+        if all the wants are True.
+        """
+        reader = Reader(wants_lection=True, wants_psalm=True,
+                        wants_believers_pray=True)
+        self.assertEqual(reader.at_least_one_wants_is_true(), True)
