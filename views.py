3bbb3bdc514049afaf6b93beec82e0c180368de6
Tomasz Galecki <tomasz.galecki@hotmail.com>
2015-09-27 23:11:10 +0200

    IndexView added

--- views.py
+++ views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-# Create your views here.
+
+class IndexView(generic.TemplateView):
+    template_name = 'lister/index.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(IndexView, self).dispatch(*args, **kwargs)
