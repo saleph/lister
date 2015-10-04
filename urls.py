0ee1768e02aeb571e42a8fa7b0b686e71e8dad04
Tomasz Galecki <tomasz.galecki@hotmail.com>
2015-10-04 14:27:21 +0200

    added reader's delete and detail views -> urls.py

--- urls.py
+++ urls.py
@@ -5,6 +5,11 @@ from . import views
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^show_readers/$', views.ShowReadersView.as_view(), name='show_readers'),
+    url(r'^reader/(?P<pk>[0-9]+)/delete_reader/$', views.DeleteReaderView.as_view(),
+        name='delete_reader'),
+    url(r'^reader/(?P<pk>[0-9]+)/$', views.ReaderDetailView.as_view(),
+        name='reader_detail'),
     url(r'^prepare_table/$', views.PrepareTableView.as_view(), name='prepare_table'),
-    url(r'^successfully_logged_out/$', views.LoggedOutView.as_view(), name='logged_out')
+    url(r'^successfully_logged_out/$', views.LoggedOutView.as_view(),
+        name='logged_out')
 ]
